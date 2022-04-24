ad172aa0917d07e6ff6ecc5874722c3e919577e9
DhanashriNerkar <dhanashrinerkar2@gmail.com>
2022-04-24 23:33:29 +0530

    UC1 Added

---
+++ src/Employee_wage_problem/Employee.java
@@ -0,0 +1,19 @@
+package Employee_wage_problem;
+
+import java.util.Random;
+
+public class Employee 
+{
+
+
+	int emphrs=0;
+	int empwage=0;
+	Random r=new Random();
+	int empcheck=r.nextInt(3);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
