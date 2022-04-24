08aa797ec18b1f587706257c8f879ce0bf41ace9
DhanashriNerkar <dhanashrinerkar2@gmail.com>
2022-04-24 23:40:17 +0530

    UC3 Added

--- src/Employee_wage_problem/Employee.java
+++ src/Employee_wage_problem/Employee.java
@@ -2,6 +2,8 @@ package Employee_wage_problem;
 
 import java.util.Random;
 
+import Day4_Assignment.Employee_wage;
+
 public class Employee
 {
 	
@@ -13,9 +15,29 @@ public class Employee
 	int empwage=0;
 	Random r=new Random();
 	int empcheck=r.nextInt(3);
+	
+	public void check()
+	{
+		switch(empcheck)
+		{
+		case part_time:
+			emphrs=4;
+			System.out.println("For Part Time Work Total Wage:");
+			System.out.println("Employee hrs:"+emphrs);
+			empwage=emphrs*empwageperhr;
+			System.out.println("Emp wage:"+empwage);
+			break;
+
+		}	
+
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Employee e=new Employee();
+		e.check();
+
 
 	}
 
